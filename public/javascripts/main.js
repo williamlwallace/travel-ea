89f625c9da30cf907f232edfb60ade4441ad1a78
Campbell <cme69@uclive.ac.nz>
2019-03-12 14:18:43 +1300

    Working post request in js to api/destinations
    - Create destination sends working json post request
    - Api Endpoint failing
    
    #implement #story[186] #refactor #testmanual

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -59,3 +59,44 @@ $('.collapse').on('show.bs.collapse', function () {
 $('.collapse').on('hide.bs.collapse', function () {
     $('#navbar').css('background-color', 'transparent');
 })
+
+function get(url) {
+    return fetch(url, {
+        method: "GET"
+    })
+}
+
+function post(url, data) {
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+function showErrors(json) {
+    console.log("yooo");
+    const elements = document.getElementsByTagName("pre");
+    for (i in elements) {
+        elements[i].innerHTML = "";
+    }
+    for (const key of Object.keys(json)) {
+        document.getElementById(key + "Error").innerHTML = json[key];
+    }
+}
+
+//insert
+function insertFieldData(json) {
+    for (const key of Object.keys(json)) {
+        if (key == "gender") {
+            document.getElementById(json[key]).checked = true;
+        } else {
+            const elements = document.getElementsByName(key);
+            for (element in elements) {
+                elements[element].value = json[key];
+            }
+        }
+    }
+}
