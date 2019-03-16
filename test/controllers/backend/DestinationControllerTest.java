2fde0a1aec711777814eca0dd9466a63b55f8077
Matthew Minish <mhm76@uclive.ac.nz>
2019-03-16 16:45:13 +1300

    Reconfigured sql scripts to allow for test-only evolutions
    #fix #story[186]

--- test/controllers/backend/DestinationControllerTest.java
+++ test/controllers/backend/DestinationControllerTest.java
@@ -1,17 +1,22 @@
 package controllers.backend;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import play.Application;
+import play.api.db.evolutions.ClassLoaderEvolutionsReader;
+import play.db.Database;
+import play.db.evolutions.Evolutions;
 import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
 import play.test.WithApplication;
 
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.OK;
@@ -19,11 +24,32 @@ import static play.test.Helpers.*;
 
 public class DestinationControllerTest extends WithApplication {
 
-    Application fakeApp;
+    private Application fakeApp;
+    private Database db;
 
     @Before
     public void setUp() {
-        fakeApp = Helpers.fakeApplication();
+        // Create custom settings that change the database to use test database instead of production
+        Map<String, String> settings = new HashMap<>();
+        settings.put("db.default.url", "jdbc:mysql://mysql2.csse.canterbury.ac.nz/seng302-2019-team400-test");
+
+        // Create a fake app that we can query just like we would if it was running
+        fakeApp = Helpers.fakeApplication(settings);
+        db = fakeApp.injector().instanceOf(Database.class);
+
+        // Clean up the evolutions that were automatically run on startup (i.e, wipe the database)
+        Evolutions.cleanupEvolutions(db);
+
+        // Only run the evolutions found in conf/test/evolutions/default instead
+        Evolutions.applyEvolutions(db, Evolutions.fromClassLoader(getClass().getClassLoader(), "test/"));
+
+        Helpers.start(app);
+    }
+
+    @After
+    public void cleanUp() {
+        Evolutions.cleanupEvolutions(db);
+        Helpers.stop(fakeApp);
     }
 
     @Test
@@ -49,7 +75,19 @@ public class DestinationControllerTest extends WithApplication {
     }
 
     @Test
-    public void createAndDeleteDestination() throws IOException {
+    public void deleteDestination() {
+        // Create request to delete newly created user
+        Http.RequestBuilder request2 = Helpers.fakeRequest()
+                .method(DELETE)
+                .uri("/api/destination/1");
+
+        // Get result and check it was successful
+        Result result2 = route(fakeApp, request2);
+        assertEquals(OK, result2.status());
+    }
+
+    @Test
+    public void createDestination() {
         // Create new json object node
         ObjectNode node = Json.newObject();
         node.put("name", "Test destination");
@@ -68,17 +106,5 @@ public class DestinationControllerTest extends WithApplication {
         // Get result and check it was successful
         Result result = route(fakeApp, request);
         assertEquals(OK, result.status());
-
-        // Get UID of newly created destination
-        Long idOfCreatedDestination = new ObjectMapper().readValue(Helpers.contentAsString(result), Long.class);
-
-        // Create request to delete newly created user
-        Http.RequestBuilder request2 = Helpers.fakeRequest()
-                .method(DELETE)
-                .uri("/api/destination/" + idOfCreatedDestination);
-
-        // Get result and check it was successful
-        Result result2 = route(fakeApp, request2);
-        assertEquals(OK, result2.status());
     }
 }
