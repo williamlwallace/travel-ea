303bd6c92bf782486429805e4f3608aa6c4c47af
Matthew Minish <mhm76@uclive.ac.nz>
2019-03-16 13:01:09 +1300

    Added annotations specifying names for all tables in DB
    #fix #story[186]

--- app/models/Passport.java
+++ app/models/Passport.java
@@ -6,11 +6,13 @@ import javax.persistence.Column;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * A class that stores information regarding users and which country passports they hold
  */
 @Entity
+@Table(name="Passport")
 public class Passport extends Model {
 
     @Id
