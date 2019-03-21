8502b745adfdf984d8a7b9ef12505eba6364d862
Campbell <cme69@uclive.ac.nz>
2019-03-21 23:51:39 +1300

    Working JWT tokens on signup and login
    #testmanual #fix #story[187]

--- app/actions/Authenticator.java
+++ app/actions/Authenticator.java
@@ -1,30 +1,32 @@
-package actions;
+// package actions;
 
-import play.mvc.Http;
-import play.mvc.Result;
-import play.mvc.Security;
+// import play.mvc.Http;
+// import play.mvc.Result;
+// import play.mvc.Security;
 
-import models.User;
+// import java.util.concurrent.CompletableFuture;
 
-public class Authenticator extends Action.Simple {  
-    public F.Promise<Result> call(Http.Context ctx) throws Throwable {
-        String token = getTokenFromHeader(ctx);
-        if (token != null) {
-              User user = User.find.where().eq("authToken", token).findUnique();
-              if (user != null) {
-                  ctx.request().setUsername(user.username);
-                  return delegate.call(ctx);
-              }
-        }
-        Result unauthorized = Results.unauthorized("unauthorized");
-        return F.Promise.pure(unauthorized);
-    }
+// import models.User;
 
-    private String getTokenFromHeader(Http.Context ctx) {
-        String[] authTokenHeaderValues = ctx.request().headers().get("X-AUTH-TOKEN");
-        if ((authTokenHeaderValues != null) && (authTokenHeaderValues.length == 1) && (authTokenHeaderValues[0] != null)) {
-            return authTokenHeaderValues[0];
-        }
-        return null;
-    }
-}
+// public class Authenticator extends Action.Simple {  
+//     public CompletableFuture<Result> call(Http.Context ctx) throws Throwable {
+//         String token = getTokenFromHeader(ctx);
+//         if (token != null) {
+//               User user = User.find.where().eq("authToken", token).findUnique();
+//               if (user != null) {
+//                   ctx.request().setUsername(user.username);
+//                   return delegate.call(ctx);
+//               }
+//         }
+//         Result unauthorized = Results.unauthorized("unauthorized");
+//         return F.Promise.pure(unauthorized);
+//     }
+
+//     private String getTokenFromHeader(Http.Context ctx) {
+//         String[] authTokenHeaderValues = ctx.request().headers().get("X-AUTH-TOKEN");
+//         if ((authTokenHeaderValues != null) && (authTokenHeaderValues.length == 1) && (authTokenHeaderValues[0] != null)) {
+//             return authTokenHeaderValues[0];
+//         }
+//         return null;
+//     }
+// }
