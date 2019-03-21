b155972ad3ff4e537b7a5bb792295d5935fe5346
Campbell <cme69@uclive.ac.nz>
2019-03-21 16:14:46 +1300

    Untested Token creation and authentication Action
    - Create authenticator Action
    - Create JWT creater method
    
    #implement #story[186]

---
+++ app/actions/Authenticator.java
@@ -0,0 +1,30 @@
+package actions;
+
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Security;
+
+import models.User;
+
+public class Authenticator extends Action.Simple {  
+    public F.Promise<Result> call(Http.Context ctx) throws Throwable {
+        String token = getTokenFromHeader(ctx);
+        if (token != null) {
+              User user = User.find.where().eq("authToken", token).findUnique();
+              if (user != null) {
+                  ctx.request().setUsername(user.username);
+                  return delegate.call(ctx);
+              }
+        }
+        Result unauthorized = Results.unauthorized("unauthorized");
+        return F.Promise.pure(unauthorized);
+    }
+
+    private String getTokenFromHeader(Http.Context ctx) {
+        String[] authTokenHeaderValues = ctx.request().headers().get("X-AUTH-TOKEN");
+        if ((authTokenHeaderValues != null) && (authTokenHeaderValues.length == 1) && (authTokenHeaderValues[0] != null)) {
+            return authTokenHeaderValues[0];
+        }
+        return null;
+    }
+}
