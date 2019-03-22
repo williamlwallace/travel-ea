5f579f0a004029021703d0734ee643959d70367a
hgc25 <hgc25@uclive.ac.nz>
2019-03-22 14:27:57 +1300

    Added directories
    #story[188]

---
+++ test/java/GenericTestRunner.java
@@ -0,0 +1,15 @@
+package java;
+
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features",
+        plugin = {"pretty", "html:target/site/cucumber-pretty", "json:target/cucumber.json"},
+        glue = "steps",
+        snippets = SnippetType.CAMELCASE)
+public class GenericTestRunner {
+
+}
---
+++ test/java/steps/SortTripsTestSteps.java
@@ -0,0 +1,53 @@
+package java.steps;
+
+public class SortTripsTestSteps {
+    @Given("I have at least two future trips")
+    public void i_have_at_least_two_future_trips() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("all my trips have at least one date")
+    public void all_my_trips_have_at_least_one_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I view my trips")
+    public void i_view_my_trips() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the trips will be displayed in chronological order based on the first date in a given trip")
+    public void the_trips_will_be_displayed_in_chronological_order_based_on_the_first_date_in_a_given_trip() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+
+    @Given("I have at least two trips")
+    public void i_have_at_least_two_trips() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("at least one trip has a date")
+    public void at_least_one_trip_has_a_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("at least one trip has no date")
+    public void at_least_one_trip_has_no_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("my trips without a date will be displayed below trips with a date")
+    public void my_trips_without_a_date_will_be_displayed_below_trips_with_a_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
