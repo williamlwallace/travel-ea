06f018b6a60a7d3d763bfef36714cb5ce18bbedf
Claudia <crf49@uclive.ac.nz>
2019-03-27 01:06:34 +1300

    Updating Edit profile  - generated options from database  - pre filled all fields except the slectors  #story[186]

--- public/javascripts/editProfile.js
+++ public/javascripts/editProfile.js
@@ -1,9 +1,94 @@
-/* Display profile dropdowns with cool tag style in profile */
-$('#nationalities').picker();
-$('#passports').picker();
-$('#travellerTypes').picker();
+var countryDict = {};
+var travellerTypeDict = {};
+
+// Runs get countries method, then add country options to drop down
+function fillCountryInfo(getCountriesUrl) {
+    // Run a get request to fetch all destinations
+    get(getCountriesUrl)
+    // Get the response of the request
+        .then(response => {
+        // Convert the response to json
+        response.json().then(data => {
+        // Json data is an array of destinations, iterate through it
+        for(let i = 0; i < data.length; i++) {
+        // Also add the item to the dictionary
+        countryDict[data[i]['id']] = data[i]['name'];
+    }
+    // Now fill the selects
+    fillNationalityDropDown();
+    fillPassportDropDown();
+});
+});
+}
+
+function fillTravellerTypes(getTravellerTypesUrl) {
+    // Run a get request to fetch all travellers types
+    get(getTravellerTypesUrl)
+    // Get the response of the request
+        .then(response => {
+        // Convert the response to json
+        response.json().then(data => {
+        // "data" should now be a list of traveller type definitions
+        // E.g data[0] = { id:1, description:"backpacker"}
+        for(let i = 0; i < data.length; i++) {
+        // Also add the item to the dictionary
+        travellerTypeDict[data[i]['id']] = data[i]['description'];
+    }
+    // Now fill the drop down box, and list of destinations
+    fillTravellerDropDown();
+
+});
+});
+}
+
+function fillNationalityDropDown() {
+    for(let key in countryDict) {
+        // For each destination, make a list element that is the json string of object
+        let item = document.createElement("OPTION");
+        item.innerHTML = countryDict[key];
+        item.value = key;
+        // Add list element to drop down list
+        document.getElementById("nationalities").appendChild(item);
+    }
+    // implements the plug in multi selector
+    $('#nationalities').picker();
+}
+
+function fillPassportDropDown() {
+    for(let key in countryDict) {
+        // For each destination, make a list element that is the json string of object
+        let item = document.createElement("OPTION");
+        item.innerHTML = countryDict[key];
+        item.value = key;
+        // Add list element to drop down list
+        document.getElementById("passports").appendChild(item);
+    }
+    // implements the plug in multi selector
+    $('#passports').picker();
+}
+
+function fillTravellerDropDown() {
+    for(let key in travellerTypeDict) {
+        // For each Traveller type, make a list element that is the json string of object
+        let item = document.createElement("OPTION");
+        item.innerHTML = travellerTypeDict[key];
+        item.value = key;
+        // Add list element to drop down list
+        document.getElementById("travellerTypes").appendChild(item);
+    }
+    // implements the plug in multi selector
+    $('#travellerTypes').picker();
+}
+
+
+/* Display gender drop down the same as the others */
 $('#gender').picker();
 
+/**
+ * The JavaScript function to process a client updating there profile
+ * @param url The route/url to send the request to
+ * @param redirect The page to redirect to if no errors are found
+ */
 function updateProfile(url, redirect) {
     // Read data from destination form
     const formData = new FormData(document.getElementById("updateProfileForm"));
@@ -12,6 +97,28 @@ function updateProfile(url, redirect) {
         ...memo,
         [pair[0]]: pair[1],
     }), {});
+    // Convert nationalities, passports and Traveller Types to Correct JSON appropriate format
+    data.nationalities = [];
+    let nat_ids = $.map($(document.getElementById("nationalities")).picker('get'),Number);
+    for (i = 0; i < nat_ids.length; i++) {
+        let nat = {};
+        nat.id = nat_ids[i];
+        data.nationalities.push(nat);
+    }
+    data.passports = [];
+    let passport_ids = $.map($(document.getElementById("passports")).picker('get'),Number)
+    for (i = 0; i < passport_ids.length; i++) {
+        let passport = {};
+        passport.id = passport_ids[i];
+        data.passports.push(passport);
+    }
+    data.travellerTypes  = [];
+    let type_ids = $.map($(document.getElementById("travellerTypes")).picker('get'),Number);
+    for (i = 0; i < type_ids.length; i++) {
+        let type = {};
+        type.id = type_ids[i];
+        data.travellerTypes.push(type);
+    }
     // Post json data to given url
     post(url,data)
         .then(response => {
@@ -27,8 +134,10 @@ function updateProfile(url, redirect) {
     });
 }
 
-function get(url) {
-    return fetch(url, {
-        method: "GET"
-    })
+function populateProfileData(profile) {
+    console.log(profile);
+    console.log(profile["nationalities"][0]["id"]);
+    $('#travellerTypes').picker('set', travellerTypes);
+    $('#nationalities').picker('set', test);
+    $('#passports').picker('set', passports);
 }
