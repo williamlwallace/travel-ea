289b88871f46dc2a32e4343bf43384e59e339e89
Claudia <crf49@uclive.ac.nz>
2019-03-26 20:57:53 +1300

    Attempting to send JSON to database  - issue mapping countrys to ids

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,6 +1,50 @@
+var countryDict = {};
+
+// Runs get countries method, then add country options to drop down
+function fillCountryInfo(getCountriesUrl) {
+    // Run a get request to fetch all destinations
+    get(getCountriesUrl)
+    // Get the response of the request
+            .then(response => {
+                // Convert the response to json
+                response.json().then(data => {
+                    // Json data is an array of destinations, iterate through it
+                    for(let i = 0; i < data.length; i++) {
+                        // Also add the item to the dictionary
+                        countryDict[data[i]['id']] = data[i]['name'];
+                    }
+                // Now fill the drop down box, and list of destinations
+                fillNationalityDropDown();
+                fillPassportDropDown();
+        });
+    });
+}
+
+function fillNationalityDropDown() {
+    for(let key in countryDict) {
+        // For each destination, make a list element that is the json string of object
+        let item = document.createElement("OPTION");
+        item.innerHTML = countryDict[key];
+        item.value = key;
+        // Add list element to drop down list
+        document.getElementById("nationalities").appendChild(item);
+    }
+    $('#nationalities').picker();
+}
+
+function fillPassportDropDown() {
+    for(let key in countryDict) {
+        // For each destination, make a list element that is the json string of object
+        let item = document.createElement("OPTION");
+        item.innerHTML = countryDict[key];
+        item.value = key;
+        // Add list element to drop down list
+        document.getElementById("passports").appendChild(item);
+    }
+    $('#passports').picker();
+}
+
 /* Display profile dropdowns with cool tag style in profile */
-$('#nationalities').picker();
-$('#passports').picker();
 $('#travellerType').picker();
 $('#gender').picker();
 /* Automatically display profile form when signing up */
@@ -21,6 +65,14 @@ function signUp(id, url, redirect) {
         ...memo,
         [pair[0]] : pair[1],
     }), {});
+    // Convert nationalities, passports and Traveller Types to Arrays
+    // if (data.nationalities) {
+    //     const temp = data.nationalities.split(",");
+    //     data.nationalities = $.map(temp,Number);
+    // }
+    data.nationalities = $.map($(document.getElementById("nationalities")).picker('get'),Number);
+    data.passports = $.map($(document.getElementById("passports")).picker('get'),Number);
+    if (data.travellerTypes) data.travellerTypes = data.travellerTypes.split(",");
 
     // Post json data to given url
     post(url, data)
