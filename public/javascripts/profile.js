27b1d9c23c8bb1d85a720bac6acd1d1ee7a293ef
Max Andrew <mla138@uclive.ac.nz>
2019-04-29 22:35:24 +1200

    Uploading and Cropping Profile Picture
    - Added Change Profile Picture modal that pops up when the profile picture is clicked
    - Fixed issue with header image blocking where the profile picture could be clicked
    - Started to integrate the Cropper library for cropping profile pictures
    
    #story [190]

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -150,7 +150,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 /**
- * The javascript method to populate the slect boxes on the edit profile scene
+ * The javascript method to populate the select boxes on the edit profile scene
  * @param url the route/url to send the request to to get the profile data
  */
 function populateProfileData(url) {
@@ -174,3 +174,27 @@ function populateProfileData(url) {
     $('#gender').picker('set', json.gender);
 });
 }
+
+
+/**
+ * Loads the cropper into the page when it loads.
+ */
+$(document).ready(function() {
+    var $image = $('#image');
+
+    $image.cropper({
+        aspectRatio: 1,
+        crop: function (event) {
+            console.log(event.detail.x);
+            console.log(event.detail.y);
+            console.log(event.detail.width);
+            console.log(event.detail.height);
+            console.log(event.detail.rotate);
+            console.log(event.detail.scaleX);
+            console.log(event.detail.scaleY);
+        }
+    });
+});
+
+// Get the Cropper.js instance after initialized
+var cropper = $image.data('cropper');
