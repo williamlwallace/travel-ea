68d2c98e36c83690ee5b3f469032798785ea03be
claudia <crf49@uclive.ac.nz>
2019-05-13 20:06:30 +1200

    Removed DropzoneJS

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -400,75 +400,6 @@ function deletePhoto(route) {
         });
 }
 
-/**
- * Sets up the dropzone properties, like having a remove button
- */
-function setupDropZone() {
-    Dropzone.options.addPhotoDropzone = {
-        acceptedFiles: '.jpeg,.png,.jpg',
-        dictRemoveFile: "remove",
-        thumbnailWidth: 200,
-        thumbnailHeight: 200,
-        dictDefaultMessage: '',
-        autoProcessQueue: false,
-
-        init: function() {
-            var submitButton = document.querySelector("#submit-all");
-            var cancelButton = document.querySelector("#remove-all");
-            var addPhotoDropzone = this;
-
-            this.on("addedfile", function () {
-                // Enable add button
-                submitButton.disabled = false;
-                submitButton.innerText = "Add"
-            });
-
-            this.on("thumbnail", function(file) {
-                // Do the dimension checks you want to do
-                if (file.width < profilePictureSize || file.height < profilePictureSize) {
-                    file.rejectDimensions()
-                }
-                else {
-                    file.acceptDimensions();
-                }
-            });
-
-            this.on("processing", function() {
-                this.options.autoProcessQueue = true;
-            });
-
-            submitButton.addEventListener("click", function() {
-                if (submitButton.innerText === "Add") {
-                    addPhotoDropzone.processQueue(); // Tell Dropzone to process all queued files.
-                    submitButton.innerText = "Done";
-                    document.getElementById("remove-all").hidden = true;
-                } else {
-                    document.getElementById("remove-all").hidden = false;
-                    addPhotoDropzone.options.autoProcessQueue = false;
-                    addPhotoDropzone.removeAllFiles();
-                    $('#uploadPhotoModal').modal('hide');
-                    fillGallery(getAllPhotosUrl);
-                }
-            });
-
-            cancelButton.addEventListener("click", function() {
-                addPhotoDropzone.removeAllFiles();
-                addPhotoDropzone.options.autoProcessQueue = false;
-                submitButton.disabled = true;
-                submitButton.innerText = "Add"
-            });
-        },
-        accept: function(file, done) {
-            file.acceptDimensions = done;
-            file.rejectDimensions = function() {
-                done("Image too small.");
-            };
-        },
-        success: function(file, response) {
-            file.serverFileName = response[0];
-        }
-    };
-}
 
 /**
  * allows the upload image button to act as an input field by clicking on the upload image file field
@@ -492,6 +423,37 @@ function uploadNewPhoto(){
   $('#cropProfilePictureModal').modal('show');
 }
 
+
+/**
+ * allows the upload image button to act as an input field by clicking on the upload image file field
+ * For a normal photo
+ */
+$("#upload-gallery-image-button").click(function() {
+    $("#upload-gallery-image-file").click();
+});
+
+/**
+ * Takes the users selected photos and  creates a form from them
+ * Sends this form to  the appropriate url
+ *
+ * @param url the appropriate  photo backend controller
+ */
+function uploadNewGalleryPhoto(url) {
+    const selectedPhotos = document.getElementById('upload-gallery-image-file').files;
+    let formData = new FormData();
+    for (let i = 0; i < selectedPhotos.length; i++) {
+        formData.append("file", selectedPhotos[i], selectedPhotos[i].name)
+    }
+    // Send request and handle response
+    postMultipart(url, formData).then(response => {
+        // Read response from server, which will be a json object
+        response.json().then(data => {
+            if (response.status === 201) {
+                fillGallery(getAllPhotosUrl);
+            }
+        })
+    })
+}
 
 /**
  * Takes a url for the backend controller method to get the users profile picture. Sends a get for this file and sets
