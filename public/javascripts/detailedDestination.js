47e7adb5af5e1433fdf006458084aeff54b765c8
Ryan <rch141@uclive.ac.nz>
2019-05-28 16:02:34 +1200

    Google maps banner for detailed destinations page - Banner is now google maps - initialises on the destination selected - Has a marker showing the destination being viewed
    #implement

--- public/javascripts/detailedDestination.js
+++ public/javascripts/detailedDestination.js
@@ -63,6 +63,7 @@ function makeDestinationPublic(destinationId) {
         if (response.status === 200) {
             $("#makeDestinationPublicModal").modal('hide');
             createPrivacyButton(true);
+            initMap(destinationId);
             toast('Destination Privacy Changed',
                 'The destination is now public.', 'success');
         } else {
@@ -259,4 +260,59 @@ function toggleLinked(guid, newLinked, destinationId) {
     }
     $("#linkPhotoToDestinationModal").modal('hide');
     window.location.href = '/destinations/' + destinationId;
+}
+
+/**
+ * Initialises google maps on detailed destinations page
+ */
+function initMap(destinationId) {
+    get(destinationRouter.controllers.backend.DestinationController.getDestination(
+        destinationId).url)
+    .then(response => {
+        // Read response from server, which will be a json object
+        response.json()
+        .then(destination => {
+            if (response.status !== 200) {
+                showErrors(destination);
+            } else {
+                // Initial map options
+                let options = {
+                    zoom: 4,
+                    center: {
+                        lat: destination.latitude - 2,
+                        lng: destination.longitude
+                    },
+                    disableDefaultUI: true,
+                    styles: [{"elementType":"labels","stylers":[{"visibility":"off"},{"color":"#f49f53"}]},{"featureType":"landscape","stylers":[{"color":"#f9ddc5"},{"lightness":-7}]},{"featureType":"road","stylers":[{"color":"#813033"},{"lightness":43}]},{"featureType":"poi.business","stylers":[{"color":"#645c20"},{"lightness":38}]},{"featureType":"water","stylers":[{"color":"#1994bf"},{"saturation":-69},{"gamma":0.99},{"lightness":43}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#f19f53"},{"weight":1.3},{"visibility":"on"},{"lightness":16}]},{"featureType":"poi.business"},{"featureType":"poi.park","stylers":[{"color":"#645c20"},{"lightness":39}]},{"featureType":"poi.school","stylers":[{"color":"#a95521"},{"lightness":35}]},{},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#813033"},{"lightness":38},{"visibility":"off"}]},{},{},{},{},{},{},{},{},{},{},{},{"elementType":"labels"},{"featureType":"poi.sports_complex","stylers":[{"color":"#9e5916"},{"lightness":32}]},{},{"featureType":"poi.government","stylers":[{"color":"#9e5916"},{"lightness":46}]},{"featureType":"transit.station","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","stylers":[{"color":"#813033"},{"lightness":22}]},{"featureType":"transit","stylers":[{"lightness":38}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#f19f53"},{"lightness":-10}]},{},{},{}]};
+
+                // New map
+                let map = new google.maps.Map(document.getElementById('map'), options);
+
+                //Setting public and private marker images, resized
+                const markerPublic = {
+                    url: 'https://image.flaticon.com/icons/svg/149/149060.svg',
+                    scaledSize: new google.maps.Size(30, 30)
+                };
+                const markerPrivate = {
+                    url: 'https://image.flaticon.com/icons/svg/139/139012.svg',
+                    scaledSize: new google.maps.Size(30, 30)
+                };
+
+                let marker = new google.maps.Marker({
+                    position: {
+                        lat: destination.latitude,
+                        lng: destination.longitude
+                    },
+                    map: map
+                });
+
+                marker.setIcon(destination.isPublic ? markerPublic
+                    : markerPrivate);
+
+                // Add marker
+                marker.setMap(map);
+
+            }
+        })
+    })
 }
