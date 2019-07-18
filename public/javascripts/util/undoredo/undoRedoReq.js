d121036a403ef18beef8be3ebffa7443478a3de6
Campbell <cme69@uclive.ac.nz>
2019-07-18 15:47:56 +1200

    Stack object

--- public/javascripts/util/undoredo/undoRedoReq.js
+++ public/javascripts/util/undoredo/undoRedoReq.js
@@ -6,9 +6,11 @@ const requestTypes = {
 
 /**
  * This acts as an interface as too what gets put on the undo redo stack
+ * @param {Object} undoReqData ReqData object for redo request
+ * @param {Object} redoReqData ReqData object for undo request
  */
 class UndoRedoReq {
-    constructor (undoReq, redoReq) {
+    constructor(undoReqData, redoReqData) {
         this.undoReq = undoReq;
         this.redoReq = redoReq;
     }
@@ -16,11 +18,41 @@ class UndoRedoReq {
 
 /**
  * Acts as an interface for what should be put insto the UndoRedoReq
+ *
+ * @param {Number} type type of request represented by requestTypes
+ * @param {String} URL address that request is ment to be delivered
+ * @param {Object} body body of request if applicable
  */
 class ReqData {
-    constructor (type, URL, body) {
+    constructor(type, URL, body={}) {
         this.type = type;
         this.URL = URL;
         this.body = body;
     }
+}
+
+/**
+ * Stack for request data
+ */
+class ReqStack {
+    constructor() {
+        this.#stack = [];
+    }
+
+    /**
+     * pushes data onto stack
+     * 
+     * @param {Object} undoRedoReq UndoRedoReq object to be pushed onto stack
+     */
+    push(undoRedoReq) {
+        this.#stack.push(undoRedoReq);
+    }
+
+    /**
+     * pops an object of the top of the stack
+     */
+     pop() {
+         //Maybe we will do stuff here?
+         return this.#stack.pop();
+     }
 }
