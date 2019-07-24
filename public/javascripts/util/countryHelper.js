cfe73e6b3ae7d1e35e17dde4b8097d84b79ab3f5
claudia <crf49@uclive.ac.nz>
2019-07-25 11:33:46 +1200

    Invalid countries are displayed with '(invalid)' in the table when adding a destination, then on the card the trip country is listed as invalid. Need to do something with the trip modal to identify an invalid country

--- public/javascripts/util/countryHelper.js
+++ public/javascripts/util/countryHelper.js
@@ -35,7 +35,7 @@ function getCountryNameById(countryCode) {
                     return data[country].name;
                 }
             }
-            return null;
+            return 'Invalid';
         });
     });
 }
