fcc4029a0dabd24cc7d6a051453c8c70e2fc6ca1
Ollie Sharplin <osh17@uclive.ac.nz>
2019-08-13 15:30:20 +1200

    Wrote JUnit tests for the updatePhotoDetails method
    - Wrote a test to check valid request details
    - Wrote a test to check an invalid caption
    - Wrote a test to check invalid tags
    - Wrote a test to check invalid photo ID
    - Wrote a test to check for a forbidden user
    
    #story[538] #test #implement

--- test/java/controllers/backend/PhotoControllerTest.java
+++ test/java/controllers/backend/PhotoControllerTest.java
@@ -2,31 +2,42 @@ package controllers.backend;
 
 import static controllers.backend.ControllersTest.adminAuthCookie;
 import static controllers.backend.ControllersTest.fakeApp;
+import static controllers.backend.ControllersTest.nonAdminAuthCookie;
+import static junit.framework.TestCase.assertTrue;
 import static org.apache.commons.io.FileUtils.getFile;
 import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.BAD_REQUEST;
+import static play.mvc.Http.Status.CREATED;
+import static play.mvc.Http.Status.FORBIDDEN;
+import static play.mvc.Http.Status.NOT_FOUND;
+import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.route;
 
 import akka.stream.javadsl.FileIO;
 import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import models.Photo;
+import models.Tag;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
 import util.objects.Pair;
 
 public class PhotoControllerTest extends ControllersTest {
-
 
     /**
      * Stop the fake app
@@ -93,7 +104,7 @@ public class PhotoControllerTest extends ControllersTest {
         // Post to url and get result, checking that a success was returned
         // Get result and check it was successful
         Result result = route(fakeApp, request);
-        assertEquals(201, result.status());
+        assertEquals(CREATED, result.status());
     }
 
     @Test
@@ -137,7 +148,7 @@ public class PhotoControllerTest extends ControllersTest {
         // Post to url and get result, checking that a success was returned
         // Get result and check it was successful
         Result result = route(fakeApp, request);
-        assertEquals(201, result.status());
+        assertEquals(CREATED, result.status());
     }
 
     @Test
@@ -148,7 +159,7 @@ public class PhotoControllerTest extends ControllersTest {
             .cookie(adminAuthCookie);
         //put and check response
         Result result = route(fakeApp, request);
-        assertEquals(200, result.status());
+        assertEquals(OK, result.status());
     }
 
     @Test
@@ -159,7 +170,7 @@ public class PhotoControllerTest extends ControllersTest {
             .cookie(adminAuthCookie);
         //put and check response
         Result result = route(fakeApp, request);
-        assertEquals(404, result.status());
+        assertEquals(NOT_FOUND, result.status());
     }
 
     @Test
@@ -170,7 +181,7 @@ public class PhotoControllerTest extends ControllersTest {
             .cookie(adminAuthCookie);
         //put and check response
         Result result = route(fakeApp, request);
-        assertEquals(404, result.status());
+        assertEquals(NOT_FOUND, result.status());
     }
 
     @Test
@@ -181,7 +192,7 @@ public class PhotoControllerTest extends ControllersTest {
             .cookie(adminAuthCookie);
         //put and check response
         Result result = route(fakeApp, request);
-        assertEquals(200, result.status());
+        assertEquals(OK, result.status());
     }
 
     @Test
@@ -192,7 +203,7 @@ public class PhotoControllerTest extends ControllersTest {
             .cookie(adminAuthCookie);
         //put and check response
         Result result = route(fakeApp, request);
-        assertEquals(404, result.status());
+        assertEquals(NOT_FOUND, result.status());
     }
 
     @Test
@@ -203,7 +214,7 @@ public class PhotoControllerTest extends ControllersTest {
             .cookie(adminAuthCookie);
         //put and check response
         Result result = route(fakeApp, request);
-        assertEquals(404, result.status());
+        assertEquals(NOT_FOUND, result.status());
     }
 
     @Test
@@ -213,14 +224,14 @@ public class PhotoControllerTest extends ControllersTest {
             .method("PUT")
             .cookie(adminAuthCookie);
         Result toggleResult = route(fakeApp, toggleRequest);
-        assertEquals(200, toggleResult.status());
+        assertEquals(OK, toggleResult.status());
 
         // Get the photo that we toggled
         Http.RequestBuilder photoRequest = Helpers.fakeRequest().uri("/api/user/1/photo")
             .method("GET")
             .cookie(adminAuthCookie);
         Result photoResult = route(fakeApp, photoRequest);
-        assertEquals(200, photoResult.status());
+        assertEquals(OK, photoResult.status());
 
         List<Photo> photos = Arrays
             .asList(
@@ -236,7 +247,7 @@ public class PhotoControllerTest extends ControllersTest {
             .method("PUT")
             .cookie(adminAuthCookie);
         Result result = route(fakeApp, toggleRequest);
-        assertEquals(404, result.status());
+        assertEquals(NOT_FOUND, result.status());
     }
 
     @Test
@@ -246,7 +257,7 @@ public class PhotoControllerTest extends ControllersTest {
             .method("GET")
             .cookie(adminAuthCookie);
         Result photoResult = route(fakeApp, photoRequest);
-        assertEquals(200, photoResult.status());
+        assertEquals(OK, photoResult.status());
 
         List<Photo> photos = Arrays
             .asList(
@@ -261,7 +272,7 @@ public class PhotoControllerTest extends ControllersTest {
             .method("GET")
             .cookie(adminAuthCookie);
         Result photoResult = route(fakeApp, photoRequest);
-        assertEquals(200, photoResult.status());
+        assertEquals(OK, photoResult.status());
 
         Photo testPhoto = new ObjectMapper().readValue(Helpers.contentAsString(photoResult), Photo.class);
         assertEquals("./public/storage/photos/test/test.jpeg", testPhoto.filename );
@@ -274,7 +285,7 @@ public class PhotoControllerTest extends ControllersTest {
             .method("GET")
             .cookie(adminAuthCookie);
         Result photoResult = route(fakeApp, photoRequest);
-        assertEquals(404, photoResult.status());
+        assertEquals(NOT_FOUND, photoResult.status());
     }
 
     @Test
@@ -284,10 +295,132 @@ public class PhotoControllerTest extends ControllersTest {
             .method("GET")
             .cookie(adminAuthCookie);
         Result photoResult = route(fakeApp, photoRequest);
-        assertEquals(200, photoResult.status());
+        assertEquals(OK, photoResult.status());
 
         Photo testPhoto = new ObjectMapper().readValue(Helpers.contentAsString(photoResult), Photo.class);
         assertEquals("test caption", testPhoto.caption );
     }
 
+    @Test
+    public void updatePhotoDetails() throws IOException {
+        Tag newTag1 = new Tag("awesome");
+        Tag newTag2 = new Tag("biking");
+        Set<Tag> tags = new HashSet<>(Arrays.asList(newTag1, newTag2));
+        String newCaption = "A new day";
+
+        Photo updateDetails = new Photo();
+        updateDetails.caption = newCaption;
+        updateDetails.tags = tags;
+
+        // Update photo
+        Http.RequestBuilder updateRequest = Helpers.fakeRequest()
+            .uri("/api/photo/1")
+            .method("PUT")
+            .cookie(adminAuthCookie)
+            .bodyJson(Json.toJson(updateDetails));
+
+        Result updateResult = route(fakeApp, updateRequest);
+        assertEquals(OK, updateResult.status());
+
+        // Check updated details
+        Http.RequestBuilder getRequest = Helpers.fakeRequest()
+            .uri("/api/photo/1")
+            .method("GET")
+            .cookie(adminAuthCookie);
+
+        Result getResult = route(fakeApp, getRequest);
+        assertEquals(OK, getResult.status());
+
+        Photo photo = new ObjectMapper().readValue(Helpers.contentAsString(getResult), Photo.class);
+
+        assertEquals(newCaption, photo.caption);
+        assertEquals(tags.size(), photo.tags.size());
+        for (Tag tag : tags) {
+            assertTrue(photo.tags.contains(tag));
+        }
+    }
+
+    @Test
+    public void updatePhotoInvalidTags() {
+        String newCaption = "A new day";
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode json = mapper.createObjectNode();
+        json.set("caption", Json.toJson(newCaption));
+
+        // Update photo
+        Http.RequestBuilder updateRequest = Helpers.fakeRequest()
+            .uri("/api/photo/1")
+            .method("PUT")
+            .cookie(adminAuthCookie)
+            .bodyJson(Json.toJson(json));
+
+        Result updateResult = route(fakeApp, updateRequest);
+        assertEquals(BAD_REQUEST, updateResult.status());
+    }
+
+    @Test
+    public void updatePhotoInvalidCaption() {
+        Tag newTag1 = new Tag("awesome");
+        Tag newTag2 = new Tag("biking");
+        Set<Tag> tags = new HashSet<>(Arrays.asList(newTag1, newTag2));
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode json = mapper.createObjectNode();
+        json.set("tags", Json.toJson(tags));
+
+        // Update photo
+        Http.RequestBuilder updateRequest = Helpers.fakeRequest()
+            .uri("/api/photo/1")
+            .method("PUT")
+            .cookie(adminAuthCookie)
+            .bodyJson(Json.toJson(json));
+
+        Result updateResult = route(fakeApp, updateRequest);
+        assertEquals(BAD_REQUEST, updateResult.status());
+    }
+
+    @Test
+    public void updatePhotoDetailsNotFound() {
+        Tag newTag1 = new Tag("awesome");
+        Tag newTag2 = new Tag("biking");
+        Set<Tag> tags = new HashSet<>(Arrays.asList(newTag1, newTag2));
+        String newCaption = "A new day";
+
+        Photo updateDetails = new Photo();
+        updateDetails.caption = newCaption;
+        updateDetails.tags = tags;
+
+        // Update photo
+        Http.RequestBuilder updateRequest = Helpers.fakeRequest()
+            .uri("/api/photo/100")
+            .method("PUT")
+            .cookie(adminAuthCookie)
+            .bodyJson(Json.toJson(updateDetails));
+
+        Result updateResult = route(fakeApp, updateRequest);
+        assertEquals(NOT_FOUND, updateResult.status());
+    }
+
+    @Test
+    public void updatePhotoDetailsForbidden() {
+        Tag newTag1 = new Tag("awesome");
+        Tag newTag2 = new Tag("biking");
+        Set<Tag> tags = new HashSet<>(Arrays.asList(newTag1, newTag2));
+        String newCaption = "A new day";
+
+        Photo updateDetails = new Photo();
+        updateDetails.caption = newCaption;
+        updateDetails.tags = tags;
+
+        // Update photo
+        Http.RequestBuilder updateRequest = Helpers.fakeRequest()
+            .uri("/api/photo/1")
+            .method("PUT")
+            .cookie(nonAdminAuthCookie)
+            .bodyJson(Json.toJson(updateDetails));
+
+        Result updateResult = route(fakeApp, updateRequest);
+        assertEquals(FORBIDDEN, updateResult.status());
+    }
 }
