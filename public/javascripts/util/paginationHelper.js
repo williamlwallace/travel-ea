3b15ae0fd5e52ae00fc170c06b7e0ce33941edbf
Max Andrew <mla138@uclive.ac.nz>
2019-08-16 23:05:45 +1200

    Completed the PaginationHelper class
    - Updated pagination CSS to disable clicking on pages that are active or disabled
    - Added JS to set the current page button to be active
    - Added JS to set the previous and next buttons to be disabled if the user is on
      the first or last page.
    
    #implement #document #story[527]

--- public/javascripts/util/paginationHelper.js
+++ public/javascripts/util/paginationHelper.js
@@ -25,51 +25,71 @@ class PaginationHelper {
 
     /**
      * Based on what page you are on, creates the correct pagination bar with up to 5 page buttons
+     * Will set the current page button to active and disable buttons that shouldn't
+     * be clicked.
      */
     drawPaginationBar() {
-        // Less than 5 pages
-        this.pageNumbers = [];
-        if(this.totalNumberPages <= 5) {
-            for(let i = 1; i <= this.totalNumberPages; i++) {
-                this.pageNumbers.push(i);
-            }
-        }
-        // In first 3 pages, and more than 5 total
-        else if(this.totalNumberPages >= 5 && this.pageNum < 3) {
-            for(let i = 1; i <= 5; i++) {
-                this.pageNumbers.push(i);
-            }
-        }
-        // In last 3, and more than 5 total
-        else if(this.totalNumberPages >= 5 && pageNum > this.totalNumberPages - 2) {
-            for(let i = this.totalNumberPages - 4; i <= this.totalNumberPages; i++) {
-                this.pageNumbers.push(i);
-            }
-        } else {
-            for(let i = this.pageNum - 2; i <= this.pageNum + 2; i++) {
-                this.pageNumbers.push(i);
-            }
-        }
-
         this.paginationObject.html("");
-        this.paginationObject.append("<li class=\"page-item previous\" style=\"cursor: pointer\"><a class=\"page-link\">Previous</a></li>");
-        this.pageNumbers.forEach((item) => {
-            this.paginationObject.append("<li class=\"page-item anotherPage\" style=\"cursor: pointer\" data=\""+item+"\"><a class=\"page-link\">" + item + " </a></li>");
-        });
-        this.paginationObject.append("<li class=\"page-item next\" style=\"cursor: pointer\"><a class=\"page-link\">Next</a></li>");
+        if (this.totalNumberPages > 1) {
+            // Less than 5 pages
+            this.pageNumbers = [];
+            if(this.totalNumberPages <= 5) {
+                for(let i = 1; i <= this.totalNumberPages; i++) {
+                    this.pageNumbers.push(i);
+                }
+            }
+            // In first 3 pages, and more than 5 total
+            else if(this.totalNumberPages >= 5 && this.pageNum < 3) {
+                for(let i = 1; i <= 5; i++) {
+                    this.pageNumbers.push(i);
+                }
+            }
+            // In last 3, and more than 5 total
+            else if(this.totalNumberPages >= 5 && this.pageNum > this.totalNumberPages - 2) {
+                for(let i = this.totalNumberPages - 4; i <= this.totalNumberPages; i++) {
+                    this.pageNumbers.push(i);
+                }
+            } else {
+                for(let i = this.pageNum - 2; i <= this.pageNum + 2; i++) {
+                    this.pageNumbers.push(i);
+                }
+            }
 
-        let paginationClassObject = this;
-        this.paginationObject.find('.previous').click(function() {
-            paginationClassObject.goToPage(paginationClassObject.pageNum - 1);
-        });
+            const paginationClassObject = this;
 
-        this.paginationObject.find('.next').click(function() {
-            paginationClassObject.goToPage(paginationClassObject.pageNum + 1);
-        });
+            //Previous button
+            const isFirstPage = (this.pageNum === 1);
+            this.paginationObject.append("<li class=\"page-item previous " +
+                (isFirstPage ? "disabled" : "") + "\" "
+                + "style=\"cursor: pointer\"><a class=\"page-link\">Previous</a></li>");
 
-        this.paginationObject.find('.anotherPage').click(function() {
-            paginationClassObject.goToPage(this.data);
-        });
+            this.paginationObject.find('.previous').click(function() {
+                paginationClassObject.goToPage(paginationClassObject.pageNum - 1);
+            });
+
+            //Numbered buttons
+            this.pageNumbers.forEach((item) => {
+                const isCurrentItem = (item === this.pageNum);
+                this.paginationObject.append("<li class=\"page-item anotherPage "
+                    + (isCurrentItem ? "active" : "") + "\" "
+                    + "style=\"cursor: pointer\" "
+                    + "data-pageId=\""+item+"\"><a class=\"page-link\">" + item + "</a></li>");
+            });
+
+            this.paginationObject.find('.anotherPage').click(function() {
+                paginationClassObject.goToPage(parseInt($(this).attr('data-pageId')));
+            });
+
+            //Next button
+            const isLastPage = (this.pageNum === this.totalNumberPages);
+            this.paginationObject.append("<li class=\"page-item next " +
+                (isLastPage ? "disabled" : "") + "\" "
+                + "style=\"cursor: pointer\"><a class=\"page-link\">Next</a></li>");
+
+            this.paginationObject.find('.next').click(function() {
+                paginationClassObject.goToPage(paginationClassObject.pageNum + 1);
+            });
+        }
     }
 
     /**
@@ -78,9 +98,7 @@ class PaginationHelper {
      * @param {Number} desiredPageNumber Page number to change to
      */
     goToPage(desiredPageNumber) {
-        //TODO: Should check if it is the first or last page and disable the buttons if needed
         if(desiredPageNumber > this.totalNumberPages || desiredPageNumber < 1) {
-            //TODO: This will be replaced by disabling buttons
             toast("No such page", "The page you have tried to go to does not exist", "danger");
         } else {
             this.pageNum = desiredPageNumber;
