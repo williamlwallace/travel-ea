0fc91adcfe34ffb1e40181187c33c9c8a5e3f0a6
claudia <crf49@uclive.ac.nz>
2019-08-16 15:16:05 +1200

    Fixed tests by updating evolutions

--- conf/test/country/evolutions/default/1.sql
+++ conf/test/country/evolutions/default/1.sql
@@ -1,5 +1,5 @@
 -- AUTHOR: Matthew Minish, William Wallace, Ollie Sharplin, what about me?
--- MODIFIED: 9/7/2019 2.00PM
+-- MODIFIED: 16/8/2019 2.00PM
 
 -- !Ups
 
@@ -119,8 +119,10 @@ CREATE TABLE IF NOT EXISTS Destination
     country_id        INT NOT NULL,
     is_public         BIT NOT NULL DEFAULT 0,
     deleted           BOOLEAN NOT NULL DEFAULT false,
+    primary_photo_id_guid  INT,
     FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
     FOREIGN KEY (country_id) REFERENCES CountryDefinition(id) ON DELETE CASCADE,
+    FOREIGN KEY (primary_photo_id_guid) REFERENCES Photo(guid) ON DELETE CASCADE,
     PRIMARY KEY (id)
   );
 
@@ -171,8 +173,10 @@ CREATE TABLE IF NOT EXISTS TripData
     destination_id    INT NOT NULL,
     arrival_time      DATETIME,
     departure_time    DATETIME,
+    primary_photo_id_guid  INT,
     FOREIGN KEY (trip_id) REFERENCES Trip(id) ON DELETE CASCADE,
     FOREIGN KEY (destination_id) REFERENCES Destination(id) ON DELETE CASCADE,
+    FOREIGN KEY (primary_photo_id_guid) REFERENCES Photo(guid) ON DELETE CASCADE,
     PRIMARY KEY (guid),
     INDEX tripdata_index (trip_id, position),
     INDEX destination_id_index (destination_id)
@@ -191,6 +195,18 @@ CREATE TABLE IF NOT EXISTS DestinationPhoto
     INDEX Destination_photo_index (photo_id, destination_id),
     UNIQUE(photo_id, destination_id)
   );
+
+-- Create PendingDestinationPhotos table, which specifies all current requests for a change in primary photos
+CREATE TABLE IF NOT EXISTS PendingDestinationPhoto
+(
+    id                  INT NOT NULL AUTO_INCREMENT,
+    guid                INT NOT NULL,
+    photo_id            INT NOT NULL,
+    destination_id      INT NOT NULL,
+    FOREIGN KEY (photo_id) REFERENCES Photo(guid) ON DELETE CASCADE,
+    FOREIGN KEY (destination_id) REFERENCES Destination(id) ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
 
 -- Create treasure hunt table, which stores the riddle and dates or a treasure hunt about a destination
 CREATE TABLE IF NOT EXISTS TreasureHunt
@@ -269,6 +285,7 @@ DROP TABLE TripTag;
 DROP TABLE DestinationTag;
 DROP TABLE Tag;
 DROP TABLE TreasureHunt;
+DROP TABLE PendingDestinationPhoto;
 DROP TABLE DestinationPhoto;
 DROP TABLE Photo;
 DROP TABLE DestinationTravellerType;
