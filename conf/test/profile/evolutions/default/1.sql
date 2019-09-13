88723056c6f4f5c4c51facc68e83d82f71a29cba
Ollie <osh17@uclive.ac.nz>
2019-09-13 16:49:08 +1200

    Fixed accidental change where FollowerDestination table foreign key referenced User table instead of Profile table
    #story[580] #fix

--- conf/test/profile/evolutions/default/1.sql
+++ conf/test/profile/evolutions/default/1.sql
@@ -145,7 +145,7 @@ CREATE TABLE IF NOT EXISTS FollowerDestination
     deleted           BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (guid),
     FOREIGN KEY (destination_id) REFERENCES Destination(id) ON DELETE CASCADE,
-    FOREIGN KEY (follower_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (follower_id) REFERENCES Profile(user_id) ON DELETE CASCADE
   );
 
 -- Create DestinationTravellerType table, which specifies the traveller types of users
