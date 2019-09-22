8d9c7a18e4f9617dc8929f867303b3d47f3a8c08
Campbell <cme69@uclive.ac.nz>
2019-09-23 11:18:07 +1200

    Changed dest link photos to use multi gallery cards

--- public/javascripts/util/newsFeed.js
+++ public/javascripts/util/newsFeed.js
@@ -127,7 +127,7 @@ const NewsFeedEventTypes = {
      * reference ID = ID of photo
      * dest ID = ID of destination
      */
-    MULTIPLE_DESTINATION_PHOTO_LINKS: multipleDestinationPhotoLinks,
+    MULTIPLE_DESTINATION_PHOTO_LINKS: multipleGalleryPhotos,
 
     /**
      * A public destination has had its primary photo updated
@@ -425,18 +425,6 @@ function newPrimaryPhotoCard(event) {
  * @param {object} event newsfeed event item data
  */
 function groupedTripUpdates(event) {
-    const card = createUserWrapperCard(event);
-    //TODO: The card
-    return card;
-}
-
-
-/**
- * Creates news Feed card for groupes of destination photos
- *
- * @param {object} event newsfeed event item data
- */
-function multipleDestinationPhotoLinks(event) {
     const card = createUserWrapperCard(event);
     //TODO: The card
     return card;
